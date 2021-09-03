4e3e6371e010f4726029ace410003362f59b63d3
Carlos  Tamayo <carlos@Carloss-Air.attlocal.net>
2021-09-02 22:01:02 -0400

    Updated site.js

--- js/site.js
+++ js/site.js
@@ -1,16 +1,23 @@
 //Testing Prism
 
 
+
+//Get values from page
+//controller function
 function getValues() {
     let startValue = document.getElementById('startValue').value;
     let endValue = document.getElementById('endValue').value;
 }
 
-
+// generate numbers from start value to end value
+//logic function(s)
 function generateNumber() {
 
 }
 
+
+//display the number and mark them even
+//display or view functions
 function displayNumbers() {
 
 }
